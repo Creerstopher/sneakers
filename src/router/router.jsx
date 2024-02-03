6d9ad3cc1659b47af572c627f063b887d2c8449a
Creerstopher <zagiduulin@vk.com>
2024-02-03 12:13:14 +0300

    first commit

---
+++ src/router/router.jsx
@@ -0,0 +1,29 @@
+import { createBrowserRouter } from "react-router-dom"
+// import CatalogPage from "../pages/CatalogPage"
+import Root from "../pages/root"
+import HomePage from "../pages/HomePage"
+import ProfilePage from "../pages/ProfilePage"
+import FavoritePage from "../pages/FavoritePage"
+
+const router = createBrowserRouter([
+    {
+        path: "/",
+        element: <Root />,
+        children: [
+            {
+                path: "/",
+                element: <HomePage />,
+            },
+            {
+                path: "/profile",
+                element: <ProfilePage />
+            },
+            {
+                path: "/favorite",
+                element: <FavoritePage />,
+            },
+        ]
+    }
+])
+
+export default router
